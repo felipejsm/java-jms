0f965d69ba141d4b28135630ffc43e629791393a
felipejsm <felipejsm41@gmail.com>
2018-03-19 22:21:37 -0300

    Aula 2 - completa

---
+++ alura/ws/jms/src/br/com/caelum/jms/TesteConsumidor.java
@@ -0,0 +1,36 @@
+package br.com.caelum.jms;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
+public class TesteConsumidor {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = factory.createConnection();
+		connection.start();
+		
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination file = (Destination) context.lookup("financeiro");
+		MessageConsumer consumer = session.createConsumer(file);
+		
+		Message message = consumer.receive();
+		
+		System.out.println("Recebendo msg: "+ message);
+		
+		new Scanner(System.in).nextLine();
+		session.close();
+		connection.close();
+		context.close();
+	}
+
+}
